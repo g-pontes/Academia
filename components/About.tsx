3620c428ef83737d6809a09d1c677f2b1c07e841
g-pontes <gui12072004@gmail.com>
2024-04-29 16:25:41 -0300

    about 1.0

--- components/About.tsx
+++ components/About.tsx
@@ -1,9 +1,36 @@
+'use client'
 
+import { FaUsers } from 'react-icons/fa';
+import { IoIosPricetag } from 'react-icons/io';
+import { FaDumbbell } from 'react-icons/fa6';
+
+import { motion } from 'framer-motion';
+import { fadeIn } from '@/lib/variants';
+
+const features = [
+  {
+    icon: <FaUsers />,
+    title: 'award-winning trainers',
+    subtitle: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ducimus perferendis accusantium fugiat quis quidem dolorum possimus deleniti corporis culpa, omnis rem dicta ratione, natus dolore optio. Dicta molestias optio nesciunt!'
+  },
+  {
+    icon: <IoIosPricetag />,
+    title: 'excellent prices',
+    subtitle: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ducimus perferendis accusantium fugiat quis quidem dolorum possimus deleniti corporis culpa, omnis rem dicta ratione, natus dolore optio. Dicta molestias optio nesciunt!'
+  },
+  {
+    icon: <FaDumbbell />,
+    title:'modern equipament',
+    subtitle:'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ducimus perferendis accusantium fugiat quis quidem dolorum possimus deleniti corporis culpa, omnis rem dicta ratione, natus dolore optio. Dicta molestias optio nesciunt!'
+  },
+]
 
 const About = () => {
   return (
-    <section className="h-screen bg-yellow-300" id="about">
-      About
+    <section className="pt-8 pb-14 lg:pt-16 lg:pb-28" id="about">
+      <div>
+        
+      </div>
     </section>
   )
 }
