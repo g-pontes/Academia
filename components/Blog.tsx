fad3e5e328b8a7b23b7ae6dc3c02083217b208cf
g-pontes <gui12072004@gmail.com>
2024-04-30 15:46:05 -0300

    blog and brands complete

--- components/Blog.tsx
+++ components/Blog.tsx
@@ -1,6 +1,151 @@
+'use client'
+
+//import swiper react components
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+//import swiper styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+
+// components
+import CustomButton from './CustomButton';
+import SwiperNavButtons from './SwiperNavButtons';
+import Image from 'next/image';
+import Link from 'next/link';
+
+import { motion } from 'framer-motion';
+import { fadeIn } from '@/lib/variants';
+
+
+const blogData = [
+  {
+    img: '/assets/img/blog/post1.jpg',
+    date: '10 de Março, 2014',
+    title: 'Mantenha uma estrutura perfeita após o treino',
+    href: '',
+  },
+  {
+    img: '/assets/img/blog/post2.jpg',
+    date: '10 de Março, 2014',
+    title: 'Mantenha uma estrutura perfeita após o treino',
+    href: '',
+  },
+  {
+    img: '/assets/img/blog/post3.jpg',
+    date: '10 de Março, 2014',
+    title: 'Mantenha uma estrutura perfeita após o treino',
+    href: '',
+  },
+  {
+    img: '/assets/img/blog/post4.jpg',
+    date: '10 de Março, 2014',
+    title: 'Mantenha uma estrutura perfeita após o treino',
+    href: '',
+  },
+  {
+    img: '/assets/img/blog/post1.jpg',
+    date: '10 de Março, 2014',
+    title: 'Mantenha uma estrutura perfeita após o treino',
+    href: '',
+  },
+  {
+    img: '/assets/img/blog/post2.jpg',
+    date: '10 de Março, 2014',
+    title: 'Mantenha uma estrutura perfeita após o treino',
+    href: '',
+  },
+  {
+    img: '/assets/img/blog/post3.jpg',
+    date: '10 de Março, 2014',
+    title: 'Mantenha uma estrutura perfeita após o treino',
+    href: '',
+  },
+  {
+    img: '/assets/img/blog/post4.jpg',
+    date: '10 de Março, 2014',
+    title: 'Mantenha uma estrutura perfeita após o treino',
+    href: '',
+  },
+]
+
 const Blog = () => {
   return (
-    <section className="h-screen bg-emerald-300" id="blog">Blog</section>
+    <section className="bg-primary-300 text-white py-24" id="blog">
+      <div className="container mx-auto">
+        <motion.h2 
+          variants={fadeIn('up', 0.4)}
+          initial='hidden'
+          whileInView={'show'}
+          viewport={{ once: false, amount: 0.2 }}
+          className='h2 text-center mb-8'
+        >Blogs</motion.h2>
+        <motion.div
+          variants={fadeIn('up', 0.6)}
+          initial='hidden'
+          whileInView={'show'}
+          viewport={{ once: false, amount: 0.2 }}
+        >
+          <Swiper
+            slidesPerView={1}
+            spaceBetween={30}
+            breakpoints={{
+              768: {
+                slidesPerView: 2,
+                spaceBetween: 15,
+              },
+              1024: {
+                slidesPerView: 3,
+              },
+              1400: {
+                slidesPerView: 4,
+              },
+            }}
+            className="h-[420px] md:max-w-[660px] lg:max-w-none mb-8"
+          >
+            {blogData.map((post, index) => {
+              return <SwiperSlide key={index}>
+                <div className="flex flex-col justify-start h-full max-w-[320px] mx-auto">
+                  <Image 
+                    src={post.img} 
+                    width={320}
+                    height={266}
+                    alt=''
+                    className="mb-6"
+                  />
+                  <div className="flex flex-col items-center">
+                    <p className="max-w-[380px] uppercase text-[12px] tracking-[3px] mb-1">
+                      {post.date}
+                    </p>
+                    <Link  
+                      className="hover:text-accent transition-all duration-300" 
+                      href={post.href}
+                    >
+                      <h5 className="h5">{post.title}</h5>
+                    </Link>
+                  </div>
+                </div>
+              </SwiperSlide>
+            })}
+            <SwiperNavButtons
+              containerStyles='absolute left-0 right-0 bottom-[16rem] w-full max-w-[370px]
+              sm:max-w-[620px] md:max-w-[960px] xl:max-w-[1320px] mx-auto 
+              z-50 flex justify-between gap-1'
+              btnStyles='bg-accent text-while w-[36px] h-[36px] flex justify-center 
+              items-center hover:bg-accent transition-all duration-300 rounded-full'
+              iconsStyles='text-[20px]'
+            />
+          </Swiper>
+        </motion.div>
+        <motion.div
+          variants={fadeIn('up', 0.6)}
+          initial='hidden'
+          whileInView={'show'}
+          viewport={{ once: false, amount: 0.2 }}
+        >
+          <CustomButton containerStyles='block w-[196px] h-[62px] mx-auto' text='Veja Mais!' />
+        </motion.div>
+      </div>
+    </section>
   )
 }
 
