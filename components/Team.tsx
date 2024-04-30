ae512b8ae86d82f971e7c3b29ed5662c629df6ee
g-pontes <gui12072004@gmail.com>
2024-04-30 10:30:22 -0300

    team complete

--- components/Team.tsx
+++ components/Team.tsx
@@ -57,27 +57,65 @@ const trainerData = [
 
 const Team = () => {
   return (
-    <section className="h-screen bg-red-300" id="equipe">
-      <div className="container mx-auto">
-        <h2>Nossos treinadores</h2>
+    <section className="py-12 xl:h-[110vh]" id="equipe">
+      <div className="container mx-auto h-full flex flex-col items-center justify-center">
+        <motion.h2 
+          variants={fadeIn('up', 0.4)}
+          initial='hidden'
+          whileInView={'show'}
+          viewport={{ once: false, amount: 0.2 }}
+          className="h2 text-center mb-6">Nossos treinadores</motion.h2>
         {/* trainers grid */}
-        <div>
+        <motion.div
+          variants={fadeIn('up', 0.6)}
+          initial='hidden'
+          whileInView={'show'}
+          viewport={{ once: false, amount: 0.2 }} 
+          className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-12 mb-12">
           {trainerData.map((trainer, index) => {
             return(
               <div
               key={index}
-              className=""
+              className="flex flex-col items-center text-center"
               >
                 <div>
                   {/* image */}
                   <div className="relative w-[320px] h-[360px] mx-auto mb-4">
-                    <Image src={trainer.description} fill alt=""/>
+                    <Image src={trainer.image} fill alt=""/>
+                  </div>
+                  {/* name */}
+                  <h4 className="h4 mb-2">{trainer.name}</h4>
+                  {/* role */}
+                  <p className="uppercase text-xs tracking-[3px] mb-2">{trainer.role}</p>
+                  {/* description */}
+                  <p className="mb-6 max-w-[320px] mx-auto">{trainer.description}</p>
+                  {/* socials */}
+                  <div className="flex gap-12 justify-center">
+                  {trainer.social.map((social, index) => {
+                    return <div key={index}>
+                      <Link
+                        className="hover:text-accent transition-all"
+                        href={social.href}
+                      >
+                        <social.icon className="text-lg" />
+                      </Link>
+                    </div>
+                  })}
                   </div>
                 </div>
               </div>
             )
           })}
-        </div>
+        </motion.div>
+        {/* btn */}
+        <motion.div
+          variants={fadeIn('up', 0.6)}
+          initial='hidden'
+          whileInView={'show'}
+          viewport={{ once: false, amount: 0.2 }}
+        >
+          <CustomButton containerStyles="w-[196px] h-[62px]" text="Veja todos!" />
+        </motion.div>
       </div>
     </section>
   )
